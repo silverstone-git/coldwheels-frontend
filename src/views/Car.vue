2168f233fc96c9dfa4a853bf15496351b43b36d0
silverstone-git <silverstone965@gmail.com>
2025-02-01 17:32:02 +0530

    fixed carousel and pagination setup

--- src/views/Car.vue
+++ src/views/Car.vue
@@ -3,6 +3,18 @@
     <div class="h-full w-full card border rounded-lg p-4">
       {{ car.Make }} {{ car.ModelName }} {{ car.EngineSize }}
       {{ car.ID }}
+    <div class="carousel w-full">
+        <div v-for="(imageURL, index) in car.ImageURLs" :id="`image-div-${index}-${car.ID}`" :key="index" class="carousel-item py-4 relative w-full">
+        <img
+          :src="imageURL"
+          class="w-full h-full bg-cover bg-center" 
+        />
+        <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a :href="`#image-div-${cycle(index, -1, car.ImageURLs.length)}-${car.ID}`" class="btn btn-circle">❮</a>
+            <a :href="`#image-div-${cycle(index, 1, car.ImageURLs.length)}-${car.ID}`" class="btn btn-circle">❯</a>
+        </div>
+      </div>
+    </div>
     </div>
   </div>
 </template>
@@ -17,5 +29,13 @@
   }
 
   const props = defineProps<Props>()
+  const cycle = (index: number, incrementor: number, size: number) => {
+    if((index + incrementor) > size - 1) {
+      return 0;
+    }  else if ((index + incrementor) < 0) {
+      return size - 1;
+    }
+    return index + incrementor;
+  }
 
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="w-full flex items-center justify-center h-[93vh] ">
-    <div class="w-9/12 h-11/12 p-8 card border shadow-xl rounded-lg">
-      <div class="card-title">All cars!!</div>
-      <div class="flex flex-wrap items-center h-full">
-        <div v-for="car in cars" :key="car.ID" class="w-4/12 h-1/3">
+    <div class="w-9/12 h-11/12 p-8 card flex flex-col border shadow-xl rounded-lg">
+      <div class="card-title h-1/12">Your Cars</div>
+      <div class="flex flex-wrap h-full overflow-hidden">
+        <div v-for="car in cars" :key="car.ID" class="w-4/12 h-1/2">
           <Car :car="car" />
         </div>
       </div>
-      <div class="w-1/2 self-center join grid grid-cols-3">
+      <div class=" self-center w-1/3 h-1/12 join grid grid-cols-3 flex items-center">
         <button @click="() => {
           if(page > 1)
             page--;
-        }" class="join-item btn btn-outline" :class="{
-          'prevDisabled': 'btn-disabled'
-        }">Previous</button>
-        <div class="join-item btn btn-outline">{{ page }}</div>
+          }" class="join-item btn btn-outline" :class="{
+            'prevDisabled': 'btn-disabled'
+          }">Previous</button>
+          <div class="join-item btn btn-outline">{{ page }}</div>
         <button @click="page++" class="join-item btn btn-outline">Next</button>
       </div>
     </div>
